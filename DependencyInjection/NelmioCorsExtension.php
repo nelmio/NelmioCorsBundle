c40101c105315a3877397e80e9a94cdf78241fb8
Aleksey Polyvanyi <alexndlm@gmail.com>
2023-04-20 13:36:59 +0200

    feature/allow-arrays-in-env-variables

--- DependencyInjection/NelmioCorsExtension.php
+++ DependencyInjection/NelmioCorsExtension.php
@@ -29,56 +29,8 @@ class NelmioCorsExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $defaults = array_merge(
-            [
-                'allow_origin' => [],
-                'allow_credentials' => false,
-                'allow_headers' => [],
-                'expose_headers' => [],
-                'allow_methods' => [],
-                'max_age' => 0,
-                'hosts' => [],
-                'origin_regex' => false,
-            ],
-            $config['defaults']
-        );
-
-        if ($defaults['allow_credentials'] && in_array('*', $defaults['expose_headers'], true)) {
-            throw new \UnexpectedValueException('nelmio_cors expose_headers cannot contain a wildcard (*) when allow_credentials is enabled.');
-        }
-
-        // normalize array('*') to true
-        if (in_array('*', $defaults['allow_origin'])) {
-            $defaults['allow_origin'] = true;
-        }
-        if (in_array('*', $defaults['allow_headers'])) {
-            $defaults['allow_headers'] = true;
-        } else {
-            $defaults['allow_headers'] = array_map('strtolower', $defaults['allow_headers']);
-        }
-        $defaults['allow_methods'] = array_map('strtoupper', $defaults['allow_methods']);
-
-        if ($config['paths']) {
-            foreach ($config['paths'] as $path => $opts) {
-                $opts = array_filter($opts);
-                if (isset($opts['allow_origin']) && in_array('*', $opts['allow_origin'])) {
-                    $opts['allow_origin'] = true;
-                }
-                if (isset($opts['allow_headers']) && in_array('*', $opts['allow_headers'])) {
-                    $opts['allow_headers'] = true;
-                } elseif (isset($opts['allow_headers'])) {
-                    $opts['allow_headers'] = array_map('strtolower', $opts['allow_headers']);
-                }
-                if (isset($opts['allow_methods'])) {
-                    $opts['allow_methods'] = array_map('strtoupper', $opts['allow_methods']);
-                }
-
-                $config['paths'][$path] = $opts;
-            }
-        }
-
         $container->setParameter('nelmio_cors.map', $config['paths']);
-        $container->setParameter('nelmio_cors.defaults', $defaults);
+        $container->setParameter('nelmio_cors.defaults', $config['defaults']);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
